0da3dfb29849468f7fa5060b8b21f9a496c59567
anthrgrnwrld <anthrgrnwrld@gmail.com>
2015-10-17 09:22:17 +0900

    Initial Commit

---
+++ customButtonWithAspectFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customButtonWithAspectFit
+//
+//  Created by Masaki Horimoto on 2015/10/17.
+//  Copyright © 2015年 Masaki Horimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
